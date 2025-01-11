d07468aa7f85b8785c2cf148d583356715a6d0f7
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-11 09:13:13 -0500

    Feat: usesCases

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,3 +1,4 @@
+export * from './movies/get-by-id.use-case';
 export * from './movies/now-playing.use-case';
 export * from './movies/popular.use-case';
 export * from './movies/top_rated.use-case';
