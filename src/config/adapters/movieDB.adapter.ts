1478ef0d39245640b6d2e18e8567a7f581e67d98
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-11 13:48:31 -0500

    Feat: configuration env

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '4b33bcae448525d328a121527e3d878f',
+    api_key: THE_MOVIE_DB_KEY ?? "no-key",
     laguage: 'es',
   },
 });
